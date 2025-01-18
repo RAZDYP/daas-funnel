024b9cefee9ed045acdb07d8459f2acb8de260c6
Chinmoy Saha <chinmoysaha321@gmail.com>
2025-01-18 13:37:03 +0530

    font updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,23 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import localFont from 'next/font/local';
+
+
+const openSauceSansMedium = localFont({
+  src: '../fonts/font-openSauceSans-Medium.ttf',
+  variable: '--font-openSauce-medium',
+});
+
+const openSauceSansRegular = localFont({
+  src: '../fonts/font-openSauceSans-Regular.ttf',
+  variable: '--font-openSauce-regular',
+});
+
+const openSauceSansSemibold = localFont({
+  src: '../fonts/font-openSauceSans-Semibold.ttf',
+  variable: '--font-openSauce-semibold',
+});
+
 
 export const metadata: Metadata = {
   title: "DaaS Funnel",
@@ -14,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`antialiased  bg-white text-black `}
+        className={`antialiased  bg-white text-black ${openSauceSansMedium.variable} ${openSauceSansRegular.variable} ${openSauceSansSemibold.variable}`}
       >
         {children}
       </body>
